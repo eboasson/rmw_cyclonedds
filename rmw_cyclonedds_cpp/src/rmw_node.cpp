4183190ed0370c66a420aa23001b0f3eea5cebdc
Chen Lihui <lihui.chen@sony.com>
2020-09-12 04:15:35 +0800

    Fix that not to delete some objects after destroying functions (#236)
    Signed-off-by: Chen.Lihui <lihui.chen@sony.com>

--- rmw_cyclonedds_cpp/src/rmw_node.cpp
+++ rmw_cyclonedds_cpp/src/rmw_node.cpp
@@ -334,8 +334,8 @@ struct client_service_id_t
 
 struct CddsCS
 {
-  CddsPublisher * pub;
-  CddsSubscription * sub;
+  std::unique_ptr<CddsPublisher> pub;
+  std::unique_ptr<CddsSubscription> sub;
   client_service_id_t id;
 };
 
@@ -3790,8 +3790,8 @@ static rmw_ret_t rmw_init_cs(
   const rosidl_service_type_support_t * type_support = get_service_typesupport(type_supports);
   RET_NULL(type_support);
 
-  auto pub = new CddsPublisher();
-  auto sub = new CddsSubscription();
+  auto pub = std::make_unique<CddsPublisher>();
+  auto sub = std::make_unique<CddsSubscription>();
   std::string subtopic_name, pubtopic_name;
   void * pub_type_support, * sub_type_support;
 
@@ -3891,8 +3891,8 @@ static rmw_ret_t rmw_init_cs(
   dds_delete(subtopic);
   dds_delete(pubtopic);
 
-  cs->pub = pub;
-  cs->sub = sub;
+  cs->pub = std::move(pub);
+  cs->sub = std::move(sub);
   return RMW_RET_OK;
 
 fail_instance_handle:
@@ -3956,6 +3956,7 @@ static rmw_ret_t destroy_client(const rmw_node_t * node, rmw_client_t * client)
   }
 
   rmw_fini_cs(&info->client);
+  delete info;
   rmw_free(const_cast<char *>(client->service_name));
   rmw_client_free(client);
   return RMW_RET_OK;
@@ -4012,6 +4013,7 @@ fail_service_name:
   rmw_client_free(rmw_client);
 fail_client:
   rmw_fini_cs(&info->client);
+  delete info;
   return nullptr;
 }
 
@@ -4058,6 +4060,7 @@ static rmw_ret_t destroy_service(const rmw_node_t * node, rmw_service_t * servic
   }
 
   rmw_fini_cs(&info->service);
+  delete info;
   rmw_free(const_cast<char *>(service->service_name));
   rmw_service_free(service);
   return RMW_RET_OK;
@@ -4112,6 +4115,7 @@ fail_service_name:
   rmw_service_free(rmw_service);
 fail_service:
   rmw_fini_cs(&info->service);
+  delete info;
   return nullptr;
 }
 
